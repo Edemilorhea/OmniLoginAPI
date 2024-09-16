26b8aded58afc8ee8b418dacc84526e4087f11ce
TC <leon8847@gmail.com>
2024-09-16 22:57:01 +0800

    新增UserHashTable

---
+++ LoginAPI/Migrations/20240916135336_UserHashData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserHashData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserHashData",
+                columns: table => new
+                {
+                    UserHashDataId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHashData", x => x.UserHashDataId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserHashData");
+        }
+    }
+}
