63bacbd8f21feaf32bc73f21c32a5924ead2d5eb
TC <leon8847@gmail.com>
2024-09-17 00:00:10 +0800

    修正回應、更改Login, Logout名稱

--- LoginAPI/Services/AccountService/IAccountService.cs
+++ LoginAPI/Services/AccountService/IAccountService.cs
@@ -5,7 +5,8 @@ public interface IAccountService
 {
     Task<ServiceResponse<User>> ValidateUser(User user);
     Task<ServiceResponse<User>> RegisterUser(User user);
-    Task<User> LoginUser(User user);
+    Task<User> Login(User user);
     Task<User> ChangePassword(User user);
-    Task<User> LogoutUser(User user);
+    Task<User> Logout(User user);
+    Task<ServiceResponse<User>> GetUserInfo(Guid id);
 }
